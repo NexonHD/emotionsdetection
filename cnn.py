5015d85a2e1213ef2440641e9bfbe0ddaba724cf
YetiTheWinner <yannick.boehm@gmx.de>
2024-05-17 18:51:04 +0200

    repaired lr_scheduler, structured cnn.py

--- cnn.py
+++ cnn.py
@@ -30,15 +30,12 @@ train_images = numpy.expand_dims(train_images, -1)
 test_images = numpy.expand_dims(test_images, -1)
 
 if LOAD_MODEL_NAME == '':
-    #model = config.create_model()
-    model = config.create_vgg_model()
+    model = config.create_model()
+    #model = config.create_vgg_model()
 else:
     model = tf.keras.models.load_model(LOAD_MODEL_NAME)
 
-
-
-with tf.device('/GPU:0'):
-
+def DataGenerators():
     datagen = ImageDataGenerator(
         rotation_range=10,
         width_shift_range=0.2,
@@ -53,28 +50,48 @@ with tf.device('/GPU:0'):
 
     train_generator = datagen.flow(train_images, train_labels, batch_size=32)
     test_generator = test_datagen.flow(test_images, test_labels, batch_size=32)
+    return train_generator, test_generator
+
+
+def get_lr_schedulers(initial_learning_rate = 0.01, end_lr = 0.000001, decay_change_steps = config.EPOCHS):
+    decay_rate = (float(end_lr/initial_learning_rate))**(1.0/decay_change_steps)
+
+    exponential_lr_schedule = tf.keras.optimizers.schedules.ExponentialDecay(
+        initial_learning_rate=initial_learning_rate,
+        decay_steps=898*config.EPOCHS // decay_change_steps,  # Anpassung der decay_steps für  langsamere Reduktion
+        decay_rate=decay_rate,
+        staircase=True
+    )
+
+    cosine_lr_schedule = tf.keras.optimizers.schedules.CosineDecay(
+        initial_learning_rate=initial_learning_rate,
+        decay_steps=898*config.EPOCHS,
+        alpha=end_lr
+    )
+
+    return exponential_lr_schedule, cosine_lr_schedule
+
+
+
+with tf.device('/GPU:0'):
+
+    train_generator, test_generator = DataGenerators()
 
     #model = tf.keras.models.load_model("C:/Users/Yannick/Codes/Emotion Detector/models/2_0-SGD_optimizer-dataaugmentation.keras")
 
-    initial_learning_rate = 0.001
-    decay_rate = 0.1
-    lr_schedule = tf.keras.optimizers.schedules.ExponentialDecay(
-        initial_learning_rate=initial_learning_rate,
-        decay_steps=10000,
-        decay_rate=decay_rate,
-        staircase=True)
+    exponential_lr_schedule, cosine_lr_schedule = get_lr_schedulers(initial_learning_rate = 0.01, end_lr = 0.000001, decay_change_steps = config.EPOCHS)
 
 
-    adam_optimizer = keras.optimizers.Adam(learning_rate=lr_schedule)
-    sgd_optimizer = keras.optimizers.SGD(learning_rate=lr_schedule, momentum=0.9)
+    adam_optimizer = keras.optimizers.Adam(learning_rate=exponential_lr_schedule)
+    sgd_optimizer = keras.optimizers.SGD(learning_rate=exponential_lr_schedule, momentum=0.9)
 
     early_stopping = keras.callbacks.EarlyStopping(monitor='val_accuracy', patience=10, restore_best_weights=True)
 
-    checkpoint_path = (config.KERAS_DIRECTORY + "bestweightsfor_" + SAVE_MODEL_NAME)  
+    checkpoint_path = (config.KERAS_DIRECTORY + "bestweightsfor_" + SAVE_MODEL_NAME)
     checkpoint_callback = ModelCheckpoint(checkpoint_path, monitor='val_accuracy', verbose=1, save_best_only=True, mode='max')  
 
-    model.compile(optimizer=sgd_optimizer, metrics=['accuracy'], loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True))
-    model.load_weights(checkpoint_path)
+    model.compile(optimizer=adam_optimizer, metrics=['accuracy'], loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True))
+    #model.load_weights(checkpoint_path)
     history = model.fit(train_generator, epochs=config.EPOCHS, validation_data=test_generator, callbacks=[checkpoint_callback])
     predictions = model.predict(test_images)
 
